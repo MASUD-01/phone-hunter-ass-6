24baf9b6ba3c7f915811f766a4546cbb762a7627
MASUD-01 <masudrana96141@gmail.com>
2022-02-28 22:46:30 +0600

    show phones display

--- javascript.js
+++ javascript.js
@@ -4,9 +4,28 @@ const loadData = () => {
     inputValue.value = '';
     fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValueText}`)
         .then(res => res.json())
-        .then(data => displayPhones(data))
+        .then(data => displayPhones(data.data))
 }
 
-const displayPhones = (phone) => {
-    console.log(phone)
+const displayPhones = (phones) => {
+    const sectionDiv = document.getElementById('section-phone')
+    sectionDiv.innerHTML = ''
+    phones.forEach(element => {
+        console.log(element)
+        const displayDiv = document.createElement('div')
+        displayDiv.classList.add('col-md-4')
+        displayDiv.classList.add('mb-5')
+        displayDiv.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${element.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="">${element.phone_name}</h5>
+                <p class="card-text">${element.brand}</p>
+                <button class="btn btn-primary">Know Details</button>
+            </div>
+        </div>`
+        sectionDiv.appendChild(displayDiv)
+
+
+    });
 }
