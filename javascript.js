6452baee48ed50e3f4d7dff57d242943031c06fd
MASUD-01 <masudrana96141@gmail.com>
2022-03-01 01:07:53 +0600

    add comment

--- javascript.js
+++ javascript.js
@@ -1,18 +1,22 @@
+//Global variable
+const errorMessage = document.getElementById("error")
+const sectionDiv = document.getElementById('section-phone')
+const showIndivisualInfo = document.getElementById('indivisual-image')
+
+//when users search phones then data is load this function
 const loadData = () => {
     const inputValue = document.getElementById('input-text')
     const inputValueText = inputValue.value
-
     inputValue.value = '';
     fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValueText}`)
         .then(res => res.json())
         .then(data => displayPhones(data.data.slice(0, 20)))
 }
-const errorMessage = document.getElementById("error")
-const sectionDiv = document.getElementById('section-phone')
-const showIndivisualInfo = document.getElementById('indivisual-image')
+
 const displayPhones = (phones) => {
+    //consition is when products is not found the storage,the message will show
     if (phones.length === 0) {
-        errorMessage.innerText = "not found product"
+        errorMessage.innerText = "no product found "
         sectionDiv.innerHTML = ''
         showIndivisualInfo.innerHTML = ''
     }
@@ -38,6 +42,7 @@ const displayPhones = (phones) => {
     }
 }
 
+//indivisual id pass this function
 const knowDisplay = (info) => {
     fetch(`https://openapi.programming-hero.com/api/phone/${info}`)
         .then(res => res.json())
@@ -52,7 +57,7 @@ const indivisualPhone = (phone) => {
         singaldiv.innerHTML = `
         <img width='400px' src='${phone.image}'/>
         <h2>${phone.name}</h2>
-        <h2>${phone.releaseDate.innerText = 'Release date not found'}</h2>
+        <h2>${phone.releaseDate.innerText = 'No ReleaseDate found'}</h2>
         `
         showIndivisualInfo.appendChild(singaldiv)
     }
